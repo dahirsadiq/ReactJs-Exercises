013f3a29930bf422b031d83a109247f34cf84907
dahirsadiq <daahirsadiiq123@gmail.com>
2025-11-27 14:29:51 +0300

    Completed exercise 3

---
+++ exercise_3/src/App.jsx
@@ -0,0 +1,18 @@
+
+import UserCard from "./UserCard";
+
+
+function App(){
+  return(
+    <>
+    <UserCard name={"Ahmed Ali Jama"}
+      email={"ahmedali34@gmail.com"}/>
+     <UserCard name={"Ali Kassim Mohamed"}
+      email={"ali435@gmail.com"}/>
+
+       <UserCard name={"Mohamed Ahmed Mohamud"}
+      email={"moah2@gmail.com"}/>
+    </>
+  )
+}
+export default App;
---
+++ exercise_3/src/UserCard.jsx
@@ -0,0 +1,12 @@
+
+
+const UserCard=({name,email})=>{
+    return(
+        <div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+
+export default UserCard;
