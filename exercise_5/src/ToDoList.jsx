0e3d0105de96c8995068d904f1dc8462b015f44d
dahirsadiq <daahirsadiiq123@gmail.com>
2025-11-30 18:06:25 +0300

    added decrease button and price total

--- exercise_5/src/ToDoList.jsx
+++ exercise_5/src/ToDoList.jsx
@@ -7,6 +7,8 @@ const ToDoList =()=>{
     const [products ,setProducts]=useState([]);
     const [name,setName]=useState("");
      const [price,setPrice]=useState();
+
+    //  Increase Quantity
      const handleIncreaseQuantity =(id)=>{
             const updatedProducts=products.map(product=>(
                 product.id ===id? {...product ,quantity:product.quantity +1}:product
@@ -14,6 +16,15 @@ const ToDoList =()=>{
              setProducts(updatedProducts);
 
         }
+        // Decrease Quantity
+        const handleDecreaseQuantity=(id)=>{
+            const updatedProducts=products.map(product=>(
+                product.id ===id && product.quantity > 0 ? {...product ,quantity:product.quantity - 1}:product
+            ))
+             setProducts(updatedProducts);
+
+         }
+
      
      const handleAddCart=()=>{
         const newProduct={
@@ -22,18 +33,16 @@ const ToDoList =()=>{
             price:price,
             quantity:1
         }
-
-        // const handleDecreaseQuantity=(id)=>{
-
-        // }
-
-        
-
         setProducts([...products ,newProduct]);
         setName("");
         setPrice("");
         
      }
+
+
+    //  calculation of total price
+    const Totalprice=products.reduce((total, product) => total + product.price * product.quantity,0);
+   
 
     return(
         <div>
@@ -48,14 +57,16 @@ const ToDoList =()=>{
                     <li key={product.id}> <strong>{product.name}</strong>
                         :{product.price}<br></br>
 
-                        Quantity:<div>
-                            <button>-</button>
-                            {product.quantity}
+                        <strong>Quantity:</strong>  <div>
+                           {product.quantity}
                             <button onClick={()=>handleIncreaseQuantity(product.id)}>+</button>
+                            <button onClick={()=>handleDecreaseQuantity(product.id)}>-</button>
                         </div>
                         </li>
                 ))}
             </ul>
+
+            <h2>the total price is{Totalprice} {}</h2>
 
 
         </div>
