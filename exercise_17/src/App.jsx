27e5fb761f5a0ce4ffc26d24d7f623934b3c6b0a
dahirsadiq <daahirsadiiq123@gmail.com>
2025-12-29 14:32:04 +0300

    Added exercise 17

---
+++ exercise_17/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ContactForm from './ContactForm'
+
+function App() {
+
+
+  return (
+    <>
+    <ContactForm />
+    
+    </>
+  )
+}
+
+export default App
